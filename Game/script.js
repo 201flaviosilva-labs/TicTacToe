0de233944bea26333a110535a3054f25dc0388cf
Flávio Silva <flavioandre.2001.9.7@gmail.com>
2020-07-18 15:10:12 +0100

    Playing Mode

--- Game/script.js
+++ Game/script.js
@@ -1,1 +1,33 @@
-alert("error");
+const boardBtn = document.querySelectorAll("body main div.board button");
+
+let numerado = [
+	"0", "1", "2",
+	"3", "4", "5",
+	"6", "7", "8"];
+
+let board = [
+	"", "", "",
+	"", "", "",
+	"", "", ""];
+const win = ["012", "345", "789", "036", "147", "258", "048", "246"];
+let points = { X: 0, O: 0 };
+let playerMove = "X";
+
+window.onload = () => main();
+function main() {
+	for (let i = 0; i < boardBtn.length; i++)
+		boardBtn[i].addEventListener("click", () => move(i));
+}
+
+function move(i) {
+	if (!board[i]) {
+		boardBtn[i].innerHTML = playerMove;
+		if (playerMove == "X") playerMove = "O";
+		else playerMove = "X";
+		checkWin();
+	} else console.log("Ocupado");
+}
+
+function checkWin() {
+	
+}
